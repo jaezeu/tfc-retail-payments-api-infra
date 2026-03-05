3eb21b3dd0a541faf48953d0f8080314b64cb058
jaezeu <jazeel.meerasah@gmail.com>
2026-03-05 23:47:03 +0800

    test trigger

--- envs/prod/storage/main.tf
+++ envs/prod/storage/main.tf
@@ -8,11 +8,13 @@ module "ddb" {
   table_name  = "${local.name}-ddb"
   hash_key    = "pk"
   enable_pitr = false
+  billing_mode = "PROVISIONED"
 
   tags = merge(var.tags, {
     business_unit = var.business_unit
     app          = var.app_name
     env          = var.env
     layer        = local.layer
+    # cost_center   = "1111"
   })
 }
--- modules/storage/dynamodb/main.tf
+++ modules/storage/dynamodb/main.tf
@@ -1,6 +1,9 @@
 resource "aws_dynamodb_table" "this" {
   name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
+  billing_mode = var.billing_mode
+  read_capacity  = var.billing_mode == "PROVISIONED" ? 20 : null
+  write_capacity = var.billing_mode == "PROVISIONED" ? 20 : null
+
   hash_key     = var.hash_key
 
   attribute {
